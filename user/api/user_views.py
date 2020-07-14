94f52e78a222921c215117d0ba825dd3ad6370f1
PradeepYendluri <nagavarapradeepyendluri@gmail.com>
2020-07-14 19:16:02 +0530

    api modified 3

--- user/api/user_views.py
+++ user/api/user_views.py
@@ -1,6 +1,6 @@
 from .serializers import *
 from rest_framework import generics
-from user.models import Profile
+#from user.models import Profile
 from django.contrib.auth.models import User
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
@@ -11,9 +11,9 @@ class UserCreateView(generics.CreateAPIView):
         serializer_class = UserSerializer
         queryset = User.objects.all()
 
-class ProfileView(generics.RetrieveUpdateAPIView):
-    class Meta:
-        serializer_class = ProfileSerializer
-        queryset = Profile.objects.all()
-        permission_classes = (IsAuthenticated,)
-        authentication_classes = (TokenAuthentication,)
+#class ProfileView(generics.RetrieveUpdateAPIView):
+#    class Meta:
+#        serializer_class = ProfileSerializer
+#        queryset = Profile.objects.all()
+#        permission_classes = (IsAuthenticated,)
+#        authentication_classes = (TokenAuthentication,)
