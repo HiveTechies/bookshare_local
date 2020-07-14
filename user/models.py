57e87b0da634cbcd55cfdf848e50aa7f4feb6887
PradeepYendluri <nagavarapradeepyendluri@gmail.com>
2020-07-14 18:29:55 +0530

    removed profile

--- user/models.py
+++ user/models.py
@@ -14,28 +14,6 @@ class Collection(models.Model):
         return self.name
 
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default="user.png", upload_to="profile_pics")
-    quote = models.CharField(max_length=80, default="n")
-    aboutme = models.TextField(max_length=150, default="n")
-
-    def __str__(self):
-        return f"{self.user.username} Profile"
-
-    def save(self, *args, **kwargs):
-        super().save()
-
-        img = Image.open(self.image.path)
-
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
-
-    def get_absolute_url(self):
-        return reverse('profile')
-
 class ExplicitReport(models.Model):
     url=models.CharField(max_length=200)
     name_of_book= models.CharField(max_length=100)
--- user/views.py
+++ user/views.py
@@ -86,29 +86,6 @@ def studio(request):
         )
     return render(request,'user/studio.html')
 
-@login_required
-def profile(request):
-    if request.method == 'POST':
-        u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(
-            request.POST, request.FILES, instance=request.user.profile)
-        if u_form.is_valid() and p_form.is_valid():
-            u_form.save()
-            p_form.save()
-            messages.success(
-                request, f'Your Account has been updated successfully!')
-            return redirect('profile')
-    else:
-        u_form = UserUpdateForm(instance=request.user)
-        p_form = ProfileUpdateForm(instance=request.user.profile)
-
-    context = {
-        'u_form': u_form,
-        'p_form': p_form
-    }
-    return render(request, 'user/profile.html', context)
-
-
 def view_user_profile(request, user_id):
     return render(request, 'user/profile.html')
 
