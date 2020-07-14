69207cfe15e45c774ffea424b031e735edab8562
PradeepYendluri <nagavarapradeepyendluri@gmail.com>
2020-07-14 19:18:59 +0530

    api modified 4

--- user/api/user_urls.py
+++ user/api/user_urls.py
@@ -2,6 +2,6 @@ from django.urls import path,include
 from .user_views import *
 
 urlpatterns = [
-    path('profile/',ProfileView.as_view()),
+#    path('profile/',ProfileView.as_view()),
     path('user_create/',UserCreateView.as_view()),
 ]
