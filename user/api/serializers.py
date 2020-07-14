01e8cf1c53605dfefb2c590820ff67e34afe47aa
PradeepYendluri <nagavarapradeepyendluri@gmail.com>
2020-07-14 18:49:00 +0530

    api modified

--- user/api/serializers.py
+++ user/api/serializers.py
@@ -1,11 +1,11 @@
 from rest_framework import serializers
-from user.models import Profile
+#from user.models import Profile
 from django.contrib.auth.models import User
 
-class ProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Profile
-        fields = ('image','country','aboutme')
+#class ProfileSerializer(serializers.ModelSerializer):
+#    class Meta:
+#        model = Profile
+#        fields = ('image','country','aboutme')
 
 class UserSerializer(serializers.ModelSerializer):
     profile = ProfileSerializer(required=True)
